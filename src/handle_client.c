2c7d92a3a1322359b1dae3adbbf5cb31877ce729
afazio1 <alexa.fazio04@gmail.com>
2025-05-11 22:14:12 -0400

    send response back

--- src/handle_client.c
+++ src/handle_client.c
@@ -14,6 +14,12 @@
 #define BUFFER_SIZE 4096
 #define CRLF "\r\n"
 
+void cleanup(char **buf) {
+  // free(NULL) no operation is performed
+  free(*buf);
+  *buf = NULL;
+}
+
 typedef struct {
   char *method;
   char *uri;
@@ -23,7 +29,7 @@ typedef struct {
 
 typedef struct {
   char *version;
-  uint32_t status;
+  char *status;
   char *reason;
 } state_line_t;
 
@@ -52,7 +58,9 @@ int parse_http_request(char * msg, http_req_t *req) {
   char *second_token = strtok_r(NULL, " ", &first_token_ptr);
   size_t second_token_len = strlen(second_token);
   req->uri = calloc(second_token_len + 1, sizeof(char));
-  if (req->uri == NULL) return 1;
+  if (req->uri == NULL) {
+    goto free_stuff;
+  }
   strncpy(req->uri, second_token, second_token_len + 1);
 
   char *field;
@@ -73,24 +81,70 @@ int parse_http_request(char * msg, http_req_t *req) {
       } else { // this is the value
         if (field == req->host) {
           req->host = calloc(token_len + 1, sizeof(char));
-          if (req->host == NULL) return 1;
+          if (req->host == NULL) {
+            goto free_stuff;
+          }
           strncpy(req->host, token, token_len + 1);
         } else if (field == req->user_agent) {
           req->user_agent = calloc(token_len + 1, sizeof(char));
-          if (req->user_agent == NULL) return 1;
+          if (req->user_agent == NULL) {
+            goto free_stuff;
+          }
           strncpy(req->user_agent, token, token_len + 1);
         }
       }
       token = strtok_r(NULL, " ", &token_ptr);
     }
   }
-  // parse subsequent lines to get key-value pair
+
   return 0;
+
+free_stuff:
+  cleanup(&req->method);
+  cleanup(&req->uri);
+  cleanup(&req->host);
+  cleanup(&req->user_agent);
+  return 1;
 }
 
-void create_http_request() {
+char * create_http_response(char **content, http_res_t *res) {
+  res->status_line.status = "200";
+  res->status_line.reason = "OK";
+  res->status_line.version = "HTTP/1.1";
 
+  if (content == NULL) {
+    res->status_line.status = "500";
+    res->status_line.reason = "Server Error";
+  }
+
+  res->content_length = strlen(*content);
+  res->content_type = "text/html";
+  
+  // create the res string
+  char* res_str = calloc(res->content_length + 100, sizeof(char));
+  strcat(res_str, res->status_line.version);
+  strcat(res_str, " ");
+  strcat(res_str, res->status_line.status);
+  strcat(res_str, " ");
+  strcat(res_str, res->status_line.reason);
+  strcat(res_str, CRLF);
+  strcat(res_str, "Content-Type: ");
+  strcat(res_str, res->content_type);
+  strcat(res_str, CRLF);
+  strcat(res_str, "Content-Length: ");
+  char* content_len_str = calloc(32, sizeof(char)); 
+  if (content_len_str == NULL) {
+    return NULL;
+  }
+  sprintf(content_len_str, "%d", res->content_length);
+  strcat(res_str, content_len_str);
+  cleanup(&content_len_str);
+  strcat(res_str, CRLF);
+  strcat(res_str, CRLF);
+  strcat(res_str, *content);
+  return res_str;
 }
+
 long get_fsize(FILE *fd) {
   if (fseek(fd, 0, SEEK_END) == -1) {
     fclose(fd);
@@ -121,12 +175,6 @@ int handle_client(int client_sock) {
     if (parse_http_request(buf, &req) == 1) {
       return 1;
     }
-    // TODO: get rid of this
-    printf("Method: %s\n", req.method);
-    printf("URI: %s\n", req.uri);
-    printf("Host: %s\n", req.host);
-    printf("User-Agent: %s\n", req.user_agent);
-    fflush(NULL);
     // get file based on uri
     char* filename = "";
     if (strncmp(req.uri, "/", 1) == 0) {
@@ -136,8 +184,8 @@ int handle_client(int client_sock) {
     snprintf(temp, sizeof(temp), "../html%s%s", req.uri, filename);
     req.uri = realloc(req.uri, strlen(temp) + 1);
     strncpy(req.uri, temp, strlen(temp) + 1);
-    printf("%s\n", req.uri);
-    fflush(NULL);
+    /*printf("%s\n", req.uri);*/
+    /*fflush(NULL);*/
     FILE *fd = fopen(req.uri, "r");
     if (fd == NULL) {
       printf("error opening file: %d\n", errno);
@@ -158,23 +206,29 @@ int handle_client(int client_sock) {
 
     // close file
     fclose(fd);
-    // TODO: remove printfs
-    printf("%s\n", page_buf);
+
+    // send a response back
+    http_res_t res = {0};
+    char* res_str = create_http_response(&page_buf, &res);
+    if (res_str == NULL) {
+      printf("error creating response: %d\n", errno);
+      return 1;
+    }
+    printf("%s\n", res_str);
     fflush(NULL);
-
-    // TODO: send a response back
-    /*int bytes_sent = send(client_sock, buf, bytes_recv, 0);*/
-    /*if (bytes_sent == -1) {*/
-    /*  return 1;*/
-    /*}*/
-
-    if (req.uri != 0) free(req.uri);
-    if (req.method != 0) free(req.method);
-    if (req.host != 0) free(req.host);
-    if (req.user_agent != 0) free(req.user_agent);
-
+    int bytes_sent = send(client_sock, res_str, strlen(res_str), 0);
+    if (bytes_sent == -1) {
+      return 1;
+    }
+    
+    cleanup(&page_buf);
+    cleanup(&res_str);
+    cleanup(&req.method);
+    cleanup(&req.uri);
+    cleanup(&req.host);
+    cleanup(&req.user_agent);
   }
-   
+  printf("Closed client socket\n");
   close(client_sock);
   return 0;
 } 
